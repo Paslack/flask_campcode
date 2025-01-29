094ead20e637e0b17cd661011462ed199e7f8f45
Paslack <phcasttro@outlook.com>
2025-01-29 16:00:37 -0300

    Deixando app seguro

---
+++ apps/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+from apps import db
+from apps.models import Contato
+
+
+class ContatoForm(FlaskForm):
+    nome = StringField('Nome', validators=[DataRequired()])
+    email = StringField('E-Mail', validators=[DataRequired(), Email()])
+    assunto = StringField('Assunto', validators=[DataRequired()])
+    mensagem = StringField('Mensagem', validators=[DataRequired()])
+    btnSubmit = SubmitField('Enviar')
+
+    def save(self):
+        contato = Contato(
+            nome=self.nome.data,
+            email=self.email.data,
+            assunto=self.assunto.data,
+            mensagem=self.mensagem.data
+        )
+
+        db.session.add(contato)
+        db.session.commit()
---
+++ apps/views.py
@@ -0,0 +1,79 @@
+from apps import app, db
+from flask import render_template, url_for, request, redirect
+
+from apps.models import Contato
+from apps.forms import ContatoForm
+
+
+@app.route('/')
+def index():
+    usuario = 'Pedro'
+    idade = 24
+
+    context = {
+        "usuario": usuario,
+        "idade": idade
+    }
+    return render_template("index.html", context=context)
+
+
+@app.route('/contato/', methods=['GET', 'POST'])
+def pagina_contato():
+    form = ContatoForm()
+    context = {}
+    if form.validate_on_submit():
+        form.save()
+        return redirect(url_for('index'))
+
+    return render_template("contato.html", context=context, form=form)
+
+
+@app.route('/contato/lista/')
+def contato_lista():
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa', '')
+
+    dados = Contato.query.order_by('data_envio')
+
+    if pesquisa != '':
+        dados = dados.filter_by(nome=pesquisa)
+
+    context = {'dados': dados.all()}
+
+    return render_template("contato_lista.html", context=context)
+
+
+@app.route('/contato/<int:id>/')
+def contatoDetail(id):
+    obj = Contato.query.get(id)
+    return render_template('contato_detail.html', obj=obj)
+
+
+
+# Formato não recomendado / Antigo
+
+@app.route('/contato_old/', methods=['GET', 'POST'])
+def pagina_old():
+    context = {}
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa')
+        print('GET:', pesquisa)
+        context.update({'pesquisa': pesquisa})
+
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        assunto = request.form['assunto']
+        mensagem = request.form['mensagem']
+
+        contato = Contato(
+            nome=nome,
+            email=email,
+            assunto=assunto,
+            mensagem=mensagem
+        )
+
+        db.session.add(contato)
+        db.session.commit()
+
+    return render_template("contato_old.html", context=context)
